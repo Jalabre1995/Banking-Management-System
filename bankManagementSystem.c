7cc632ecabab1c0ac0ac30ae1eadee88d8f2ad59
Joshua Alabre <57151108+Jalabre1995@users.noreply.github.com>
2021-08-06 23:07:16 -0400

    Changes

--- bankManagementSystem.c
+++ bankManagementSystem.c
@@ -139,4 +139,14 @@ void new_acc()
     scanf("%d/%d/%d", add.dob.month,&add.dob.day, &add.dob.year);
     printf("\nEnter the age: ");
     scanf("%d", &add.age);
+    printf("\nEnter the address: ");
+    scanf("%s", add.address);
+    printf("\nEnter the citizenship number:");
+    scanf("%s",add.citizenship );
+    printf("\nEnter the phone number");
+    scanf("%lf", &add.phone);
+    printf("\nEnter the amount to deposit:$");
+    scanf("%f", &add.amt);
+    printf("\nType of account: \nt#Saving\n\t#Current\n\t#Fixed1( for 1 year\n\t#Fixed2(for 2 years)\n\t#Fixed3(for 3 years)\n\tEnter your choice:");
+    scanf("%s", add.acc_type);
 }
